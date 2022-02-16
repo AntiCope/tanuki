77012faf308ad135e3fd4c9d4bede67aa56d2e62
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-02-16 13:51:35 +0100

    updater

---
+++ src/main/java/anticope/tanuki/mixin/TitleScreenMixin.java
@@ -0,0 +1,22 @@
+package anticope.tanuki.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.util.math.MatrixStack;
+
+import anticope.tanuki.utils.UpdateUtil;
+import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
+
+@Mixin(value = TitleScreen.class)
+public class TitleScreenMixin {
+    @Inject(method = "render", at = @At("TAIL"))
+    private void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo info) {
+        MeteorExecutor.execute(() -> {
+            UpdateUtil.checkUpdate();
+        });
+    }
+}
